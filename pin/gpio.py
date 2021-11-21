3faca3478ba19ac0d5d38699b115fbe0dcc3ae70
waro163 <waro163@163.com>
2021-11-21 17:00:53 +0800

    update gpio

--- pin/gpio.py
+++ pin/gpio.py
@@ -1,6 +1,16 @@
 from machine import Pin
 
+# http://docs.micropython.org/en/latest/library/machine.Pin.html
+
+# Pin.IN, Pin.OUT, Pin.OPEN_DRAIN, Pin.ALT, Pin.ALT_OPEN_DRAIN
+# None, Pin.PULL_UP, Pin.PULL_DOWN
 p0 = Pin(0, Pin.IN, Pin.PULL_UP)
+
+p0.mode()
+p0.mode(Pin.IN)
+
+p0.on()
+p0.off()
 
 p2 = Pin(2, Pin.OUT)
 p2.off()
